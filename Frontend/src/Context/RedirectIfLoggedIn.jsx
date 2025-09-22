7e926edb72e1993ef159fafdddbca88f82eba59b
Medipelly Manvith Kumar <m6783321@gmail.com>
2025-09-22 19:32:07 +0530

    Frontend updates

--- Frontend/src/Context/RedirectIfLoggedIn.jsx
+++ Frontend/src/Context/RedirectIfLoggedIn.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { Navigate } from "react-router-dom";
 import axios from "axios";
+import { RingLoader } from "react-spinners";
 
 const RedirectIfLoggedIn = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(null); // null = loading
@@ -12,7 +13,12 @@ const RedirectIfLoggedIn = ({ children }) => {
       .catch(() => setIsLoggedIn(false));
   }, []);
 
-  if (isLoggedIn === null) return <div style={{height:"100vh",width:"100vw",backgroundColor:"red"}}>Loading...</div>; // optional loading state
+  if (isLoggedIn === null) return 
+  <div style={{height:"100vh",width:"100vw",display:"flex",justifyContent:"center",alignItems:"center"}}>
+    <RingLoader
+    color="#008325"
+    size={80}/>
+    Loading...</div>; // optional loading state
   if (isLoggedIn) return <Navigate to="/dashboard" />;
 
   return children;
